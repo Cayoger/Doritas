cd9b67e7eba1100427ed041d2fc546c6288e7da7
Jhonathan Steven Calderon Sanchez <jhonathan19920912@outlook.com>
2016-06-23 14:00:56 -0500

    Comienzo
    Aquí subo el logín sin

---
+++ html/index/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <link rel="stylesheet" href="view/css/style.css" media="screen" charset="utf-8">
+  <link rel="stylesheet" href="view/font/css/font-awesome.css" media="screen" title="no title" charset="utf-8">
+  <body>
+
+    <div class="backimg">
+
+    </div>
+    <div class="login">
+      <div class="logolog"></div>
+      <input type="text" id="user" placeholder="Cuenta"/>
+      <input type="password" id="pass" placeholder="Contraseña"/>
+      <input type="button" id="login" value="Iniciar">
+      <a href="?view=registro">Registrar!</a>
+    </div>
+
+    <footer>
+      <div>
+        <i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by ADSI 117
+      </div>
+    </footer>
+
+  </body>
+</html>
