60c2e8a672bee023c17068b6a32c1f7f685e01e0
Jhonathan Steven Calderon Sanchez <jhonathan19920912@outlook.com>
2016-07-01 22:15:32 -0500

    core

--- core/core.php
+++ core/core.php
@@ -8,7 +8,7 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','doritas');
 #Constantes de la APP
-define('APP_URL','http://localhost/repositorios/Doritas/');
+define('APP_URL','http://localhost/Doritas/');
 define('APP_TITLE','Doritas S.A.S.');
 define('APP_FOOTER','<i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by ADSI 117 - Copyright &copy; ' . date('Y',time()));
 #constantes de phpmailer
